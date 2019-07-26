401a105236404eaa48544d89509b25f5b2a302dd
wade wilson <LayneThompson@gmail.com>
2019-07-26 10:50:47 -0500

    second

--- src/components/Wizard.js
+++ src/components/Wizard.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './App.css';
 
-function App() {
+
+function Wizard() {
   return (
     <div className="App">
   
@@ -9,4 +9,4 @@ function App() {
   );
 }
 
-export default App;
+export default Wizard;
